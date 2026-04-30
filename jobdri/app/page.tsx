36f6f7069bcf4dcd302e1c3704614d9594d5c024
이윤서 <yyoo04@naver.com>
2026-04-30 17:22:49 +0900

    Initial Setting

---
+++ jobdri/app/page.tsx
@@ -0,0 +1,18 @@
+// app/page.tsx
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-white">
+      <main className="flex flex-col items-center gap-8">
+        <h1 className="text-h28-bold text-black">타이틀 텍스트 (28px, bold)</h1>
+
+        <p className="text-b16-med text-zinc-600">본문 텍스트 (16px, medium)</p>
+
+        <button className="text-btn16-semibold bg-black text-white px-6 py-3 rounded-full">
+          버튼 텍스트(16px, semibold)
+        </button>
+      </main>
+    </div>
+  );
+}
