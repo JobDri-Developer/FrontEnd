93df781de4984c782ce909f42b8e78dca04205f4
이윤서 <yyoo04@naver.com>
2026-05-03 18:43:25 +0900

    Feat: #1 디자인 시스템 설정 - 그리드, radius

--- jobdri/app/(main)/layout.tsx
+++ jobdri/app/(main)/layout.tsx
@@ -1,7 +1,5 @@
 // layout.tsx 수정 예시
 import type { Metadata } from "next";
-import "./globals.css";
-
 export const metadata: Metadata = {
   title: "JobDri",
   description: "취준생을 위한 채용 통합 관리 솔루션",
@@ -13,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko" className="h-full antialiased">
+    <html lang="ko" className="h-full antialiased grid-base container-default">
       {/* 폰트를 CSS에서 body에 직접 적용했으므로 클래스는 비워둬도 됩니다 */}
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
