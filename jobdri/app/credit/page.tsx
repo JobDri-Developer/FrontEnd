85a70307e87c3e7b83b1b1a780d26ac09f79df23
이윤서 <yyoo04@naver.com>
2026-05-10 16:48:41 +0900

    Feat: 레이아웃 수정 및 적용 [JDDEV-74]

--- jobdri/app/credit/page.tsx
+++ jobdri/app/credit/page.tsx
@@ -1,20 +1,13 @@
 import { CreditCard } from "@/components/common/cards";
-import Lnb from "@/components/common/lnb/Lnb";
-import PageHeader from "@/components/common/PageHeader";
 
 export default function CreditPage() {
   return (
     <div className="flex flex-row">
-      <Lnb />
-      <div className="flex flex-col py-11 px-10 bg-line-neutral-default w-full h-screen">
-        <PageHeader />
-        <section className="flex flex-row gap-4">
-          {" "}
-          <CreditCard />
-          <CreditCard />
-          <CreditCard />
-        </section>
-      </div>
+      <section className="flex flex-row gap-4 w-full">
+        <CreditCard />
+        <CreditCard />
+        <CreditCard />
+      </section>
     </div>
   );
 }
--- jobdri/app/layout.tsx
+++ jobdri/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
-import "./globals.css"; // 디자인 시스템과 폰트가 담긴 CSS를 반드시 임포트해야 합니다.
+import "./globals.css";
+import Lnb from "@/components/common/lnb/Lnb";
+import PageHeader from "@/components/common/PageHeader";
 
 export const metadata: Metadata = {
   title: "JobDri",
@@ -13,7 +15,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex bg-line-neutral-assistive">
+        <aside className="h-screen sticky top-0 shrink-0">
+          <Lnb />
+        </aside>
+        <main className="flex-1 min-w-0 flex flex-col content-center max-w-270 h-screen mx-auto py-11 px-10">
+          <div>
+            <PageHeader />
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
