4b9c27eb7400fe2574601d2bfa04bfacc441521e
이윤서 <yyoo04@naver.com>
2026-05-12 12:45:47 +0900

    Mod: [JDDEV-62] 레이아웃 라우팅 조건부 수정

--- jobdri/src/constants/routes.ts
+++ jobdri/src/constants/routes.ts
@@ -3,4 +3,14 @@ export const ROUTES = {
   LOGIN: "/login",
   SIGNIN: "/signin",
   MOCK_APPLY: "/mock_apply",
+  APPLY: "/apply",
+  CREDIT: "/credit",
 } as const;
+
+// LNB + Header를 표시할 경로 목록
+export const LAYOUT_ROUTES: string[] = [
+  ROUTES.HOME,
+  ROUTES.MOCK_APPLY,
+  ROUTES.APPLY,
+  ROUTES.CREDIT,
+];
