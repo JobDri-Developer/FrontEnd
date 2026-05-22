1d3e048abadc5e1a892674af05efa6e78990421a
이윤서 <yyoo04@naver.com>
2026-05-23 04:30:49 +0900

    Feat: #64 CLOSE_S 추가

--- jobdri/src/components/common/icons/Icon.tsx
+++ jobdri/src/components/common/icons/Icon.tsx
@@ -20,6 +20,7 @@ import IC_ARROW_R_N from "@/assets/ic_Arrow_Right_M.svg";
 import IC_DOTS_M from "@/assets/ic_Dots_M.svg";
 import IC_KABAB from "@/assets/ic_Kabab.svg";
 import IC_CLOSE_M from "@/assets/ic_Close_M.svg";
+import IC_CLOSE_S from "@/assets/ic_Close_S.svg";
 import IC_SIDEBAR from "@/assets/ic_SidebarToggle.svg";
 import IC_HOME_S from "@/assets/ic_Home_s.svg";
 import IC_APPLY from "@/assets/ic_Apply.svg";
@@ -64,13 +65,13 @@ const iconMap = {
   DOTS_M: IC_DOTS_M,
   KABAB: IC_KABAB,
   CLOSE_M: IC_CLOSE_M,
+  CLOSE_S: IC_CLOSE_S,
   SIDEBAR: IC_SIDEBAR,
   HOME_S: IC_HOME_S,
   APPLY: IC_APPLY,
   EX_S: IC_EX_S,
   ARROW_R_N_S: IC_ARROW_R_N_S,
   DOT_S: IC_DOT_S,
-  CLOSE: IC_CLOSE,
   EX_LINK: IC_EX_LINK,
   TOKEN: IC_TOKEN,
   SPARKLE: IC_SPARKLE,
