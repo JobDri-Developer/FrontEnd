b33a4ef7b8af770053a1f9c96ea57ebb21eca05e
mingo <mingo@mingoui-MacBookAir.local>
2026-07-28 10:22:53 +0900

    Fix: 이용약관 및 개인정보 날짜 수정 [JDDEV-98]

---
+++ jobdri/src/components/common/modal/index.ts
@@ -0,0 +1,5 @@
+export { default as ModalInput } from "./ModalInput";
+export { default as ModalFileUpload } from "./ModalFileUpload";
+export { default as ModalLinkInputDemo } from "./ModalLinkInputDemo";
+export { default as ModalNotice } from "./ModalNotice";
+export { default as ModalAdd } from "./ModalAdd";
